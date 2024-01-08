e3268b13921e882e41d2b9b7e90c2ea770f88128
Fearless Crazy <ali.taha.celik@gmail.com>
2024-01-08 19:54:30 +0300

    Update numberToOrdinalRank.js

--- src/numberToOrdinalRank.js
+++ src/numberToOrdinalRank.js
@@ -7,8 +7,25 @@ module.exports = function numberToOrdinalRank(number) {
 
     // Check the accuracy of the value in the entered parameters
     if (typeof number !== "number") throw new TypeError("The entered \"number\" value must be a Number value!");
-    if (number < 0) throw new RangeError("The entered \"number\" must be a positive Number value!");
+    if (number <= 0) throw new RangeError("The entered \"number\" must be a positive Number value!");
 
-    const ordinalRanks = ["", "1st", "2nd", "3rd"];
-    return ordinalRanks[number] || `${number}th`;
+    // If number is between 11 and 13, the suffix is "th"
+    if (number > 10 && number < 14) return `${number}th`;
+
+    // Get last digit of number
+    const lastDigit = number % 10;
+
+    switch (lastDigit) {
+        case 1:
+            return `${number}st`;
+
+        case 2:
+            return `${number}nd`;
+
+        case 3:
+            return `${number}rd`;
+
+        default:
+            return `${number}th`;
+    }
 }
